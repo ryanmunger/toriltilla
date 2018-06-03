ce88c8c2fcf82e99c466d42911c96d7f93441a94
Ryan Munger <ryanmunger@Ryans-MacBook-Pro.local>
2018-06-02 20:49:26 -0400

    Change stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    pathPrefix: '/toriltilla'
+    title: 'Gatsby Default Starter'
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
